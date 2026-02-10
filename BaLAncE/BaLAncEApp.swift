ffc0ad7c8cf29b8db1570fe5018e954d1dba9abd
Deepanshu Gaur <dgaur4575@gmail.com>
2026-02-10 03:28:42 -0500

    Initial Commit

---
+++ BaLAncE/BaLAncEApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaLAncEApp.swift
+//  BaLAncE
+//
+//  Created by applelab02 on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct BaLAncEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
